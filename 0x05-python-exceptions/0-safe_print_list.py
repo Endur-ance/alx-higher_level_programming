5d6db550e5ff3e3a459beba151d4d4f233b7beeb
Nwatu endurance <endurancenwatu9@gmail.com>
2023-01-23 13:37:09 -0800

    error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
-    total = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
+        total = 0
+            for i in range(x):
+            try:
+             print("{}".format(my_list[i]), end="")
             total += 1
-        except IndexError:
+             except IndexError:
             break
-        print("")
-        return (total)
+                                                                            print("")
+                                                                                return (total)
