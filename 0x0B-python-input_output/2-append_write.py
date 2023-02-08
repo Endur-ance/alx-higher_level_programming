50531a50cc176e3b1496350c7f7999a8045d43fd
Nwatu endurance <endurancenwatu9@gmail.com>
2023-02-08 00:12:17 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,28 +1,15 @@
-#!/usr/bin/python3 
-# 12-student.py 
-"""Defines a class Student.""" 
+#!/usr/bin/python3 #
+4-append_write.py
+"""Defines a file-appending function."""
 
-class Student: 
-    """Represent a student."""
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student. last_name (str): 
-                The last name of the student. 
-                age (int): The age of the student. 
-                """ 
-                self.first_name = first_name 
-                self.last_name = last_name 
-                self.age = age
-            def to_json(self, attrs=None): 
-                """Get a dictionary representation of the Student. 
-                If attrs is a list of strings,
-    represents only those attributes 
-    included in the list. 
-    Args: 
-        attrs (list): (Optional) The attributes to represent. """ 
-        if (type(attrs) == list and 
-                all(type(ele) == str for ele in attrs)): 
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
 
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)} 
-return self.__dict__    
+    Args:
+    filename (str): The name of the file to append to.
+    text (str): The string to append to the file.
+    Returns:
+    The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
